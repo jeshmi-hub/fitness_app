b1bba91b8b7b6c95ee00dfeb0a7b572c0a7793ea
jeshmi-hub <rajakjeshmi19@gmail.com>
2023-05-01 19:01:19 +0545

    Youtube video fetched and similar fitness suggesstions integrated

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -7,9 +7,21 @@ export const exerciseOptions = {
     }
   };
 
+export const youtubeOptions = {
+    method: 'GET',
+    params: {
+      id: 'UCE_M8A5yxnLfW0KghEeajjw'
+    },
+    headers: {
+      'X-RapidAPI-Key': '622859381emsh809e1605f972a70p169a32jsn3bcee8711af7',
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+    }
+  };
+
 export const fetchData = async(url, options) =>{
     const response = await fetch(url, options);
     const data = await response.json();
 
     return data;
 }
+
